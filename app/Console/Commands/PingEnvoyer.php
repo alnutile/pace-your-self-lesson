c6eb17ab69bc931d52db55983bf21fbf90403e4f
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2022-10-10 15:50:57 -0400

    add ping

---
+++ app/Console/Commands/PingEnvoyer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class PingEnvoyer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'heartbeat';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Envoyer Heartbeat';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Http::get('http://beats.envoyer.io/heartbeat/' . env("ENVOYER_PING"));
+    }
+}
